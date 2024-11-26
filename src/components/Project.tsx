20d36b14a555c788e0d3e90d3d0acdb5acd42731
Honningbolden <hannibalmunch@gmail.com>
2024-11-26 15:23:05 +0100

    Update Project component: add overflow-hidden class to container div

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,7 +3,7 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 
 export default function DeployedProject({ blok }: DeployedProjectProps) {
   return (
-    <div {...storyblokEditable(blok)} >
+    <div {...storyblokEditable(blok)} className="overflow-hidden">
       <h3 >{blok.preview.name}</h3>
       <iframe src={blok.netlify_url.url} className="aspect-square w-[500px] overflow-hidden" style={{ width: "500px", height: "500px" }}></iframe>
     </div>
