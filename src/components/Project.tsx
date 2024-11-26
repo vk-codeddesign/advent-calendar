ea476e4515934e40b500c3dc219331501aa3348b
Honningbolden <hannibalmunch@gmail.com>
2024-11-26 15:20:25 +0100

    Update Project component: adjust iframe styling and improve code formatting

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,8 +3,8 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 
 export default function DeployedProject({ blok }: DeployedProjectProps) {
   return (
-    <div {...storyblokEditable(blok)}>
-      <h3>{blok.preview.name}</h3>
+    <div {...storyblokEditable(blok)} >
+      <h3 >{blok.preview.name}</h3>
       <iframe src={blok.netlify_url.url} className="aspect-square w-[500px] overflow-hidden" style={{ width: "500px", height: "500px" }}></iframe>
     </div>
   )
