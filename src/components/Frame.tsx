f8f8462bbe3d82bfd3fbcec65ddb4934a4e24ac3
Honningbolden <hannibalmunch@gmail.com>
2024-11-29 13:49:34 +0100

    Remove unused date logic in Frame component to simplify code

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -38,7 +38,7 @@ export default function Frame({ blok, letter }: FrameComponentProps) {
   const frameDay = blok.dayNumber;
 
   // Get current day of december
-  const today = new Date();
+  // const today = new Date();
   // const currentMonth = today.getMonth();
   // const currentDay = currentMonth == 11 ? today.getDate() : 0;
   const currentDay = 11 == 11 ? 17 : 0;
