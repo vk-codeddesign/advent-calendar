9780565953c2dc09a44c31a8fd5d652e547441f3
Honningbolden <hannibalmunch@gmail.com>
2024-12-19 23:16:33 +0100

    Refactor Home component for code consistency and formatting improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import { fetchData } from "@/utils/FetchData";
-import { StoryblokStory } from "@storyblok/react/rsc";
-import { CanvasSnowOverlay } from "@/components/CanvasSnowOverlay";
+import { fetchData } from '@/utils/FetchData';
+import { StoryblokStory } from '@storyblok/react/rsc';
+import { CanvasSnowOverlay } from '@/components/CanvasSnowOverlay';
 
 export default async function Home() {
   const { data } = await fetchData();
 
-  const bridgeOptions = { resolveRelations: ['article.author'] }
+  const bridgeOptions = { resolveRelations: ['article.author'] };
+
+  // Adding this stupid comment to force refresh for låge 19 hahah
 
   return (
-    <main className="flex items-center justify-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
+    <main className='flex items-center justify-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]'>
       <CanvasSnowOverlay />
-      <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className='w-full h-full flex flex-col items-center justify-center'>
         <StoryblokStory story={data.story} bridgeOptions={bridgeOptions} />
       </div>
     </main>
